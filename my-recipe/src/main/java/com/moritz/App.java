122dc5583a09c837e94245006d48378b00dfcabb
Hepha1stos <moritz.brunner@gmx.de>
2024-10-18 15:01:14 +0200

    implemented basic Java files

--- my-recipe/src/main/java/com/moritz/App.java
+++ my-recipe/src/main/java/com/moritz/App.java
@@ -4,10 +4,14 @@ package com.moritz;
  * Hello world!
  *
  */
+
+import com.moritz.db.DBManager;
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        DBManager t = new DBManager();
+        
     }
 }
